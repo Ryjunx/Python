1d1a613e56c8a75586c2dca61099d652f8c8c296
Grandonial <mushabaqilsulaeman@gmail.com>
2025-06-26 12:00:56 +0700

    Update file list.py

---
+++ python/Teori/tupple.py
@@ -0,0 +1,18 @@
+tupple = (1, "ryjunix", 4, 5, "solotre")
+print(tupple)
+
+tupple_1 = (1, 6, 7, 8)
+
+for i in (tupple):
+    print("array;", i)
+
+print("------------------------------------------")
+
+for i in range(0, len(tupple)):
+    print("tuple:", tupple[i])
+
+
+n = "solotre"
+if n in tupple_1:
+    print("benar", n, "ada di tupple")
+    exit()
